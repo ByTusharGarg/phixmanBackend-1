581959f2bbc1b7e8928689094fcfe350a0fdda78
Akshitaarya98 <84074746+Akshitaarya98@users.noreply.github.com>
2022-11-04 15:16:34 +0530

    swagger added

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3276,6 +3276,26 @@ router.delete("/delete-partner-account", async (req, res) => {
   }
 })
 
+/**
+ * @openapi
+ * /admin/wallet/customer:
+ *  delete:
+ *    summary: used to fetch wallet and transactions details
+ *    tags:
+ *    - Admin Routes
+ *    responses:
+ *      500:
+ *          description: if internal server error occured while performing request.
+ *          content:
+ *            application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                  message:
+ *                    type: string
+ *                    description: a human-readable message describing the response
+ *                    example: Error encountered.
+ */
 router.get("/wallet/customer", checkAdmin, async (req, res) => {
   try {
     const {
