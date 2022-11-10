bffd6ff1b5b862f8cd5a4696f9fe581e0d4fcaf6
Akshitaarya98 <84074746+Akshitaarya98@users.noreply.github.com>
2022-11-10 18:06:24 +0530

    API to disable offer

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3181,4 +3181,49 @@ router.get("/wallet/customer",
         });
       }
     })  
+
+/**
+ * @openapi
+ * /admin/offer/disable:
+ *  get:
+ *    summary: used to disable order
+ *    tags:
+ *    - Admin Routes
+ *    responses:
+ *      500:
+ *          description: if internal server error occured while performing request.
+ *          content:
+ *            application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                  message:
+ *                    type: string
+ *                    description: a human-readable message describing the response
+ *                    example: Error encountered.
+ */    
+  router.get("/offer/disable", checkAdmin,async(req,res)=>{
+      try{
+        const {
+          query: {
+            promoCode
+          }
+        }= req;
+        const disableOffer = await Coupon.findOneAndUpdate(
+          {promoCode},
+          {
+            $set:{ isActive: false }
+          },
+          {new:true}
+        )
+        if(!disableOffer) return res.status(409).json({message: 'Unable to disable offer'})
+        
+        return res.status(200).json({message:"Offer disable", data: disableOffer})
+      } 
+      catch (error) {
+          return res.status(500).json({
+            message: "Error encountered while trying to create offer.",
+          });
+        }
+      }) 
 module.exports = router;
