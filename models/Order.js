4a1834e960e0514d121362a11224b81ac8f87afa
Tushar Garg <toshug61@gmail.com>
2022-11-20 20:15:46 +0530

    fix

--- models/Order.js
+++ models/Order.js
@@ -10,60 +10,63 @@ const Schema = mongoose.Schema;
 
 const Order = mongoose.model(
   "Order",
-  new Schema({
-    Partner: { type: Schema.Types.ObjectId, ref: "Partner", default: null },
-    Customer: { type: Schema.Types.ObjectId, ref: "Customer" },
-    OrderId: { type: String, unique: true, index: true },
-    invoiceId: { type: String, default: null },
-    OrderType: { type: String, enum: orderTypes },
-    Status: {
-      type: String,
-      enum: orderStatusTypes,
-    },
-    paidamount: { type: Number, default: 0 },
-    OrderDetails: {
-      Amount: { type: Number },
-      Gradtotal: { type: Number },
-      Items: [
+  new Schema(
+    {
+      Partner: { type: Schema.Types.ObjectId, ref: "Partner", default: null },
+      Customer: { type: Schema.Types.ObjectId, ref: "Customer" },
+      OrderId: { type: String, unique: true, index: true },
+      invoiceId: { type: String, default: null },
+      OrderType: { type: String, enum: orderTypes },
+      Status: {
+        type: String,
+        enum: orderStatusTypes,
+      },
+      paidamount: { type: Number, default: 0 },
+      OrderDetails: {
+        Amount: { type: Number },
+        Gradtotal: { type: Number },
+        Items: [
+          {
+            CategoryId: { type: Schema.Types.ObjectId, ref: "category" },
+            ModelId: { type: Schema.Types.ObjectId, ref: "Model" },
+            ServiceId: { type: Schema.Types.ObjectId, ref: "Product_Service" },
+            Cost: { type: Number },
+          },
+        ],
+      },
+      Date: { type: String },
+      PaymentMode: { type: String, enum: paymentModeTypes },
+      PaymentStatus: { type: String, enum: paymentStatus },
+      PendingAmount: { type: Number },
+      TxnId: [{ type: Schema.Types.ObjectId, ref: "ordertransaction" }],
+      address: {
+        street: String,
+        city: String,
+        pin: String,
+        state: String,
+        country: String,
+        cood: {
+          lattitude: String,
+          longitude: String,
+        },
+      },
+      statusLogs: [
         {
-          CategoryId: { type: Schema.Types.ObjectId, ref: "category" },
-          ModelId: { type: Schema.Types.ObjectId, ref: "Model" },
-          ServiceId: { type: Schema.Types.ObjectId, ref: "Product_Service" },
-          Cost: { type: Number },
+          status: { type: String, enum: orderStatusTypes },
+          timestampLog: { type: Date },
         },
       ],
-    },
-    Date: { type: String },
-    PaymentMode: { type: String, enum: paymentModeTypes },
-    PaymentStatus: { type: String, enum: paymentStatus },
-    PendingAmount: { type: Number },
-    TxnId: [{ type: Schema.Types.ObjectId, ref: "ordertransaction" }],
-    address: {
-      street: String,
-      city: String,
-      pin: String,
-      state: String,
-      country: String,
-      cood: {
-        lattitude: String,
-        longitude: String,
+      refundStatus: { type: String, default: null },
+      refundId: { type: Schema.Types.ObjectId, ref: "refund", default: null },
+      PickUpRequired: { type: Boolean },
+      payoutId: { type: Schema.Types.ObjectId, ref: "payouts" },
+      timeSlot: {
+        day: { type: String, default: "" },
+        time: { from: String, to: String },
       },
     },
-    statusLogs: [
-      {
-        status: { type: String, enum: orderStatusTypes },
-        timestampLog: { type: Date }
-      }
-    ],
-    refundStatus: { type: String, default: null },
-    refundId: { type: Schema.Types.ObjectId, ref: "refund", default: null },
-    PickUpRequired: { type: Boolean },
-    payoutId: { type: Schema.Types.ObjectId, ref: "payouts" },
-    timeSlot: {
-      day: { type: String, default: "" },
-      time: { type: String, default: "" },
-    },
-  }, { timestamps: true })
+    { timestamps: true }
+  )
 );
 
 module.exports = Order;
