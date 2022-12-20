7ad17da9ace1cec29ee1c06f0d8c1e3fc4f1fbf2
Tushar Garg <toshug61@gmail.com>
2022-12-20 18:12:53 +0530

    bank verification fixes

--- routes/payoutsRoutes.js
+++ routes/payoutsRoutes.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const Payouts = require("../libs/payments/payouts");
-const checkPartner = require('../middleware/AuthPartner');
-const partnerBankDetailsModel = require('../models/partnerbankDetails');
+const checkPartner = require("../middleware/AuthPartner");
+const partnerBankDetailsModel = require("../models/partnerbankDetails");
 const { Partner } = require("../models");
 const Cashfree = require("cashfree-sdk");
 var crypto = require("crypto");
 var shasum = crypto.createHash("sha256");
-
 
 /**
  * @openapi
@@ -42,21 +41,20 @@ var shasum = crypto.createHash("sha256");
  *    - bearerAuth: []
  */
 router.get("/verifydetails", checkPartner, async (req, res) => {
-    const { bankAccount, ifsc } = req.query;
+  const { bankAccount, ifsc } = req.query;
 
-    if (!bankAccount || !ifsc) {
-        return res.status(400).json({ message: "bankAccount ifsc are required" });
-    }
+  if (!bankAccount || !ifsc) {
+    return res.status(400).json({ message: "bankAccount ifsc are required" });
+  }
 
-    try {
-        const { data } = await Payouts.verifyBankAccountCashfree(bankAccount, ifsc);
-        return res.status(200).json({ message: "verification result.", data });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Error encountered." });
-    }
+  try {
+    const { data } = await Payouts.verifyBankAccountCashfree(bankAccount, ifsc);
+    return res.status(200).json({ message: "verification result.", data });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error encountered." });
+  }
 });
-
 
 /**
  * @openapi
@@ -95,40 +93,49 @@ router.get("/verifydetails", checkPartner, async (req, res) => {
  *    - bearerAuth: []
  */
 router.post("/create-update-bankdetails", checkPartner, async (req, res) => {
-    const partnerId = req.partner._id;
-    const { name, phone, bankAccount, ifsc, address1 } = req.body;
-    const benId = `BEN_${partnerId}`;
+  const partnerId = req.partner._id;
+  const { name, phone, bankAccount, ifsc, address1 } = req.body;
+  const benId = `BEN_${partnerId}`;
 
-    try {
-        const partnerDetails = await Partner.findById(partnerId);
-        if (!partnerDetails) {
-            return res.status(404).json({ message: "no partner found" });
-        }
-
-        const isDetailsExist = await partnerBankDetailsModel.findOne({ partnerId });
-        const payload = {
-            email: partnerDetails.email,
-            name,
-            phone,
-            bankAccount,
-            ifsc,
-            address1: partnerDetails.address.street || "N/A"
-        };
-
-
-        if (!isDetailsExist) {
-            await Payouts.createBeneficiaryToCashhfree({ beneId: benId, ...payload });
-            await Payouts.createUpdateAccountDetails(partnerId, { beneId: benId, ...payload });
-            return res.status(200).json({ message: "bank details created successfully." });
-        }
-
-        await Payouts.updateBeneficiaryToCashfree(isDetailsExist['beneId'], { beneId: isDetailsExist['beneId'], ...payload });
-        await Payouts.createUpdateAccountDetails(partnerId, { beneId: isDetailsExist['benId'], ...payload });
-        return res.status(200).json({ message: "bank details updated successfully." });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Error encountered." });
+  try {
+    const partnerDetails = await Partner.findById(partnerId);
+    if (!partnerDetails) {
+      return res.status(404).json({ message: "no partner found" });
     }
+
+    const isDetailsExist = await partnerBankDetailsModel.findOne({ partnerId });
+    const payload = {
+      email: partnerDetails.email,
+      name,
+      phone: partnerDetails.phone,
+      bankAccount,
+      ifsc,
+      address1: partnerDetails.address.street || "N/A",
+    };
+
+    if (!isDetailsExist) {
+      await Payouts.createBeneficiaryToCashhfree({ beneId: benId, ...payload });
+      await Payouts.createUpdateAccountDetails(partnerId, {
+        beneId: benId,
+        ...payload,
+      });
+      return res
+        .status(200)
+        .json({ message: "bank details created successfully." });
+    }
+    await Payouts.updateBeneficiaryToCashfree(isDetailsExist["beneId"], {
+      beneId: isDetailsExist["beneId"],
+      ...payload,
+    });
+
+    await Payouts.createUpdateAccountDetails(partnerId, payload);
+    return res
+      .status(200)
+      .json({ message: "bank details updated successfully." });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error encountered." });
+  }
 });
 
 /**
@@ -154,16 +161,15 @@ router.post("/create-update-bankdetails", checkPartner, async (req, res) => {
  *    - bearerAuth: []
  */
 router.get("/mywithdrawal", checkPartner, async (req, res) => {
-    const partnerId = req.partner._id;
-    try {
-        const mypayouts = await Payouts.myWithdrawals(partnerId);
-        return res.status(200).json({ message: "my payouts", mypayouts });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Error encountered." });
-    }
+  const partnerId = req.partner._id;
+  try {
+    const mypayouts = await Payouts.myWithdrawals(partnerId);
+    return res.status(200).json({ message: "my payouts", mypayouts });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error encountered." });
+  }
 });
-
 
 /**
  * @openapi
@@ -188,16 +194,17 @@ router.get("/mywithdrawal", checkPartner, async (req, res) => {
  *    - bearerAuth: []
  */
 router.get("/mybankdetails", checkPartner, async (req, res) => {
-    const partnerId = req.partner._id;
-    try {
-        const details = await partnerBankDetailsModel.findOne({ partnerId: partnerId });
-        return res.status(200).json({ message: "my payouts", details });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Error encountered." });
-    }
+  const partnerId = req.partner._id;
+  try {
+    const details = await partnerBankDetailsModel.findOne({
+      partnerId: partnerId,
+    });
+    return res.status(200).json({ message: "my payouts", details });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error encountered." });
+  }
 });
-
 
 /**
  * @openapi
@@ -232,67 +239,75 @@ router.get("/mybankdetails", checkPartner, async (req, res) => {
  *    - bearerAuth: []
  */
 router.post("/withdrawPayout", checkPartner, async (req, res) => {
-    const { payoutId, orderId } = req.body;
-    const partnerId = req.partner._id;
+  const { payoutId, orderId } = req.body;
+  const partnerId = req.partner._id;
 
-    if (!payoutId || !orderId) {
-        return res.status(400).json({ message: "payoutId or orderId are required" });
-    }
+  if (!payoutId || !orderId) {
+    return res
+      .status(400)
+      .json({ message: "payoutId or orderId are required" });
+  }
 
-    try {
-        await Payouts.initiatePayout(partnerId, orderId, payoutId);
-        return res.status(200).json({ message: "payouts initiates" });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: error.message || "Error encountered." });
-    }
+  try {
+    await Payouts.initiatePayout(partnerId, orderId, payoutId);
+    return res.status(200).json({ message: "payouts initiates" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: error.message || "Error encountered." });
+  }
 });
 
-
 router.post("/webhook", async (req, res) => {
-    const { signature } = req.body;
-    delete req.body.signature;
+  const { signature } = req.body;
+  delete req.body.signature;
 
-    let obj = req.body;
+  let obj = req.body;
 
-    const sortedKeys = Object.keys(obj)
-        .sort()
-        .reduce((accumulator, key) => {
-            accumulator[key] = obj[key];
-            return accumulator;
-        }, {});
+  const sortedKeys = Object.keys(obj)
+    .sort()
+    .reduce((accumulator, key) => {
+      accumulator[key] = obj[key];
+      return accumulator;
+    }, {});
 
-    const arrayData = [];
+  const arrayData = [];
 
-    for (const key in sortedKeys) {
-        let obj = {};
-        obj[key] = req.body[key];
-        arrayData.push(obj);
-    }
+  for (const key in sortedKeys) {
+    let obj = {};
+    obj[key] = req.body[key];
+    arrayData.push(obj);
+  }
 
-    let postData = "";
+  let postData = "";
 
-    let valuesArray = Object.values(sortedKeys);
+  let valuesArray = Object.values(sortedKeys);
 
-    // console.log(arrayData);
-    // console.log(valuesArray);
+  // console.log(arrayData);
+  // console.log(valuesArray);
 
-    valuesArray.map((ele) => {
-        postData += ele;
-    });
+  valuesArray.map((ele) => {
+    postData += ele;
+  });
 
+  const encoded = Buffer.from(postData, "utf8").toString("base64");
 
-    const encoded = Buffer.from(postData, 'utf8').toString('base64');
-
-    try {
-        console.log(req.body);
-        const resp = await Cashfree.Payouts.VerifySignature(req.body, signature, process.env.PAYOUT_CLIENT_SECRET);
-        console.log(resp);
-        // await Payouts.initiatePayout(partnerId, orderId, payoutId);
-        // return res.status(200).json({ message: "payouts initiates" });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: error.message || "Error encountered." });
-    }
+  try {
+    console.log(req.body);
+    const resp = await Cashfree.Payouts.VerifySignature(
+      req.body,
+      signature,
+      process.env.PAYOUT_CLIENT_SECRET
+    );
+    console.log(resp);
+    // await Payouts.initiatePayout(partnerId, orderId, payoutId);
+    // return res.status(200).json({ message: "payouts initiates" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: error.message || "Error encountered." });
+  }
 });
 module.exports = router;
